490db24791935ab3092bb6bff45f401f26d32c74
BenitalSivakumar <91796134+BenitalSivakumar@users.noreply.github.com>
2021-10-04 12:25:09 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,16 @@
+package Program1.pro;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+
+		int n=0;
+		if(n>0)
+			System.out.println("the number is postive");
+		else if(n<0)
+			System.out.println("the number is negative");
+		else
+			System.out.println("the number is neither positive nor negative");
+	}
+
+}
---
+++ PostiveToNegative.java
@@ -0,0 +1,13 @@
+package Program1.pro;
+
+public class PostiveToNegative {
+	
+public static void main(String[] args)
+{
+	int num=-40,num1 = 0;
+	if(num<0)
+		num1=num*-1;
+	System.out.println("the number is " +num +" is converted to negative "+num1);
+	
+}
+}
