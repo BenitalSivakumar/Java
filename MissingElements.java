b8ea1ed8e390b6549b57b13e809987afd118e8f9
BenitalSivakumar <91796134+BenitalSivakumar@users.noreply.github.com>
2021-10-08 15:51:21 +0530

    Add files via upload

---
+++ MissingElements.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class MissingElements {
+public static void main(String[] args) {
+	int arr[]={1,2,3,4,7,6,8};
+	Arrays.sort(arr);
+	
+	for (int i = 1; i < arr.length; i++) {
+
+		if(i+1!=arr[i])
+		{
+			i++;
+			System.out.println(i);
+			break;
+		}
+		
+	
+} 
+}
+}
+ 
